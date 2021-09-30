d2bd926f28bdd75333a3c024141f901b13a7b320
Dru <druashley@gmail.com>
2021-09-30 11:39:48 -0700

    hard coding the password to troubleshoot issue.

--- core/settings.py
+++ core/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'djongo',
         'NAME': 'protocol-alpha',
         'CLIENT': {
-            'host': 'mongodb+srv://' + str(os.getenv('DATABASE_USERNAME')) + ':' + str(os.getenv('DATABASE_PASSWORD')) + '@cluster0.utmv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            'host': 'mongodb+srv://digitalocean:8fZFnnnDkU3d7L2K@cluster0.utmv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         }
     }
 }
